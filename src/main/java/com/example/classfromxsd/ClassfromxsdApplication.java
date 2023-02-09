ec477b07d819cf27ba8182b32c5f7894132973e8
ismail.zabiulla <ismail.zabiulla@valtech.com>
2023-02-09 11:25:47 +0530

    dws

---
+++ src/main/java/com/example/classfromxsd/ClassfromxsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.classfromxsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassfromxsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassfromxsdApplication.class, args);
+	}
+
+}
